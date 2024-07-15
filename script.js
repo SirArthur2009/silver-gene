b8a9cde98790549b6b5c0e5cb0e722aecf551bbb
SirArthur2009 <160642802+SirArthur2009@users.noreply.github.com>
2024-07-15 15:46:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById("go-to-silver-genes").addEventListener(click, function(){
     //Go to Silver gene
+    window.location.href = window.location.href + "silvergene/The Silver Gene.html"
 })
